c38f4df91aa9d5c626fb571e7a1cd6ef0113b684
Trey D. <Treyzania@gmail.com>
2013-06-19 16:05:08 -0400

    Almost finished Plotter Alpha
    That was hard.  Speak up if any errors are noticed.

--- src/com/treyzania/api/games/games3d/render/plotting/PlotterProjectionPerspective_Alpha.java
+++ src/com/treyzania/api/games/games3d/render/plotting/PlotterProjectionPerspective_Alpha.java
@@ -1,6 +1,7 @@
 package com.treyzania.api.games.games3d.render.plotting;
 
 import com.treyzania.api.games.Point3;
+import com.treyzania.api.games.Vector3;
 import com.treyzania.api.games.Vector3PRY;
 import com.treyzania.api.games.games2d.Point;
 
@@ -25,9 +26,52 @@ public class PlotterProjectionPerspective_Alpha extends Plotter implements IFull
 	@Override
 	public Point plot(Point3 point3d) {
 		
-		// TODO Rewrite the algorithm in "http://en.wikipedia.org/wiki/3D_projection#Perspective_projection".
+		/*
+		 * Here is a TON of values,
+		 * don't ask to use arrays for each grouping.
+		 *  
+		 *  TODO Make an explanation of the different values
+		 *  
+		 *  TODO Finish defining the "theta" values and the "e" values.
+		 *  
+		 */
 		
-		return point3d; // All this will do is some very bad stuff.
+		// Initialize the initial values.
+		double aX = point3d.x;
+		double aY = point3d.y;
+		double aZ = point3d.z;
+		double cX = this.viewpoint.myPool.location.x;
+		double cY = this.viewpoint.myPool.location.y;
+		double cZ = this.viewpoint.myPool.location.z;
+		double thetaX = 0;
+		double thetaY = 0;
+		double thetaZ = 0;
+		double eX = 0;
+		double eY = 0;
+		double eZ = 0;
+		
+		// Define the point cache.
+		double dX;
+		double dY;
+		double dZ;
+		
+		// Define the final points.
+		double bX;
+		double bY;
+		
+		// First step in calculations.
+		dX = Math.cos(thetaY) * ( Math.sin(thetaZ) * ( aY - cY ) + Math.cos(thetaZ) * ( aX - cX ) ) - Math.sin(thetaY) * ( aZ - cZ );
+		dY = Math.sin(thetaX) * ( Math.cos(thetaY) * ( aZ - cZ ) + Math.sin(thetaY) * ( Math.sin(thetaZ) * ( aY - cY ) + Math.cos(thetaZ) * ( aX - cX ) ) ) + Math.cos(thetaX) * ( Math.cos(thetaZ) * ( aY - cY ) - Math.sin(thetaZ) * ( aX - cX ) );
+		dZ = Math.cos(thetaX) * ( Math.cos(thetaY) * ( aZ - cZ ) + Math.sin(thetaY) * ( Math.sin(thetaZ) * ( aY - cY ) + Math.cos(thetaZ) * ( aX - cX ) ) ) + Math.sin(thetaX) * ( Math.cos(thetaZ) * ( aY - cY ) - Math.sin(thetaZ) * ( aX - cX ) );
+		
+		// Second step in calculations.
+		bX = ( ( eZ / dZ ) * dX ) - eX;
+		bY = ( ( eZ / dZ ) * dY ) - eY;
+		
+		// Return the values
+		return new Point(bX, bY);
+		
+		/* sWhew... */
 		
 	}
 
